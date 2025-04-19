b8c1c83a709be5336a449eac3b4c91974b9da4e3
Wind156x <watchawin.thx@gmail.com>
2025-04-19 10:56:50 +0700

    Fix: App definition and import/export

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return <h1>Hello from App!</h1>;
+}
 
 export default App;
