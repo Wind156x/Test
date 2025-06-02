615bcc7a8fbc74a22785bbbd00e9131b39c8e4fa
Wind156x <watchawin.thx@gmail.com>
2025-06-02 18:10:57 +0700

    Update ManageIndicatorsPage.tsx

--- components/pages/ManageIndicatorsPage.tsx
+++ components/pages/ManageIndicatorsPage.tsx
@@ -1,3 +1,4 @@
+// wind156x/test/test-00f0b0a36b4a6fbeb81ded40c72bcedf036dae9a/components/pages/ManageIndicatorsPage.tsx
 // This file is no longer used and can be deleted.
 // Functionality is now part of ManageIndicatorsModal.tsx, opened from ManageSubjectsPage.tsx.
 import React from 'react';
