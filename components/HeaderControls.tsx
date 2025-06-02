05af01e29ef2750d9e28b6806d1eaf8fd5681172
Wind156x <watchawin.thx@gmail.com>
2025-06-02 18:08:45 +0700

    Update HeaderControls.tsx

--- components/HeaderControls.tsx
+++ components/HeaderControls.tsx
@@ -1,3 +1,4 @@
+// wind156x/test/test-00f0b0a36b4a6fbeb81ded40c72bcedf036dae9a/components/HeaderControls.tsx
 // This file is no longer used and can be deleted.
 // Its functionalities are now part of ScoresPage.tsx and the main App layout.
 import React from 'react';
