f7d1f6462d9e22e40a2073b8d999daa0d357985f
Wind156x <watchawin.thx@gmail.com>
2025-06-02 18:10:19 +0700

    Update SettingsModal.tsx

--- components/modals/SettingsModal.tsx
+++ components/modals/SettingsModal.tsx
@@ -1,3 +1,4 @@
+// wind156x/test/test-00f0b0a36b4a6fbeb81ded40c72bcedf036dae9a/components/modals/SettingsModal.tsx
 // This file is no longer used and can be deleted.
 // Its functionalities are now part of SettingsPage.tsx.
 import React from 'react';
